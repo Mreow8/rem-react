4ed361330f5989f8ad28f5edb309e358ffd87c70
Mreow8 <honzelchavez100@gmail.com>
2024-12-27 16:58:17 +0800

    dffdg

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../css/homes.css";
-import backgroundImage from "../assets/girlreading.webp";
 
+import backgroundImage from "../assets/girlreading.webp";
+import "../css/homes.css";
 const Home = () => {
   return (
     <div className="home-page">
