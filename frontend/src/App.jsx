83976ec7ae4c8b39e805a954f7403497948e7159
Mreow8 <honzelchavez100@gmail.com>
2024-12-28 08:24:19 +0800

    dffdg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home";
 import Products from "./components/products";
 import Signup from "./components/Signup";
+import Seller from "./components/sellers";
 import Login from "./components/login";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/products" element={<Products />} />{" "}
         <Route path="/login" element={<Login />} />{" "}
         <Route path="/signup" element={<Signup />} />{" "}
+        <Route path="/seller" element={<Seller />} />{" "}
       </Routes>
     </Router>
   );
