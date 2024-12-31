80de1e46223e8d391bd677d672d4b120a9888829
Mreow8 <honzelchavez100@gmail.com>
2024-12-31 09:41:39 +0800

    dffdgwewer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import SellerProfile from "./components/seller_profile";
 import Product_desc from "./components/product_desc";
 import Add_Product from "./components/addproducts";
 import Carts from "./components/carts";
+import Profile from "./components/profile";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
         <Route path="/sellerprofile/:id" element={<SellerProfile />} />
         <Route path="/addproducts" element={<Add_Product />} />
         <Route path="/add_to_cart" element={<Carts />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
