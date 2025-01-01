b8b9034d04e1f1595f61d0a2660efd5d1b9e6d15
Mreow8 <honzelchavez100@gmail.com>
2025-01-01 11:26:41 +0800

    dffdgwewer

--- frontend/src/components/profile.jsx
+++ frontend/src/components/profile.jsx
@@ -376,7 +376,7 @@ const App = () => {
           ) : (
             <div className="no-notifications">
               <img
-                src="/assets/no-new.png"
+                src="./assets/no-new.png"
                 alt="No Notifications"
                 className="no-notifications-image"
               />
